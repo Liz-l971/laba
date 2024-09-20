928c5bd7d47aa452735e68ec0953ee3f3c597e70
Liz_I971 <ivanovaliza0053@gmail.com>
2024-09-21 00:03:49 +0300

    main_paige

---
+++ assets/js/mainSlider.js
@@ -0,0 +1,27 @@
+// console.log(document.querySelector(".slider__cards__content").offsetLeft)
+
+const sliderContainer =document.querySelector(".slider__cards__content");
+const sliderCard = document.querySelector(".slider__card ")
+// const sliderContainerLeft = Number(document.querySelector(".slider__cards__content").offsetLeft);
+
+function nextSlide(){
+
+    let sliderContainerLeft = Number(document.querySelector(".slider__cards__content").offsetLeft);
+    if((Math.abs(sliderContainerLeft))< (Number(sliderContainer.offsetWidth)-(Number(sliderCard.offsetWidth)*3))){
+        let left = sliderContainerLeft-Number((sliderCard.offsetWidth)*2);
+        sliderContainer.style.left=left+"px";
+    }
+
+}
+
+function prevSlide(){
+
+    let sliderContainerLeft = Number(document.querySelector(".slider__cards__content").offsetLeft);
+    if(sliderContainerLeft>0){
+        let left = sliderContainerLeft+Number((sliderCard.offsetWidth)*2);
+        sliderContainer.style.left=left+"px";
+    }else{
+        sliderContainer.style.left=0+"px";
+    }
+
+}
