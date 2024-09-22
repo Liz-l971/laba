ef0fcb4ba1c80af773ec4527d6feec173312f84d
Liz_I971 <ivanovaliza0053@gmail.com>
2024-09-23 00:46:38 +0300

    normal-video

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,26 @@
+let scrollX = 0;
+let scrollY = 0;
+
 function openModal(){
     document.querySelector('.sign-in__modal').classList.toggle('open');
-    
+    scrollX= window.pageXOffset;
+    scrollY= window.pageYOffset;
     document.querySelector('.sign-in__modal__content').classList.toggle('open');
 }
 
+function openBurger(){
+    document.querySelector('.dark-background').classList.toggle('open');
+    scrollX= window.pageXOffset;
+    scrollY= window.pageYOffset;
+    document.querySelector('.burger-menu__wrap').classList.toggle('open');
+}
+
 window.addEventListener('scroll', (e) => {
+
     if (document.querySelector('.sign-in__modal').classList.contains('open')) {
-        window.scrollTo(0,0);
+
+        window.scrollTo(scrollX,scrollY);
     } 
 
   });
+
